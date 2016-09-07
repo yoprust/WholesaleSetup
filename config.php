f55c8b78704873f377bbdf263ecf79763b659c7a
yoprust <anthony_pereira@sbcglobal.net>
2016-09-07 08:40:24 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,9 @@
+<?php
+require_once('vendor/autoload.php');
 
+$stripe = array(
+  "secret_key"      => "sk_test_D0mjGvX5C6jQad8ffgDOp1Qh",
+  "publishable_key" => "pk_test_7LxXo0RV9TXL1LwzV4MuPF9h"
+);
+
+\Stripe\Stripe::setApiKey($stripe['secret_key']);
