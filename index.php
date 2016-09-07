cbc49b6220937eeb8c0ebd42ac83b24e3febde0f
yoprust <anthony_pereira@sbcglobal.net>
2016-09-07 08:51:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <form action="charge.php" method="post">
   <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
           data-key="<?php echo $stripe['publishable_key']; ?>"
-          data-description="Access for a year"
+          data-description="Initial Purchase"
           data-amount="5000"
           data-locale="auto"></script>
 </form>
